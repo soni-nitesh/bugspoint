c383304b8f61fb21fa06aed224d304d54b10a4f8
TechWizzz <kb94132@gmail.com>
2019-07-13 14:05:57 +0530

    cards

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,36 @@
 <template>
 <div>
-    <v-layout style='margin-top:50px'>
+    <v-layout style='margin-top:50px' row>
       <google-map />
-    </v-layout>
+       <v-flex xs12 md3 pa-3 ma-3>
+       <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Bug Title</div>
+            <span class="grey--text">Descririon</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+   
+          <v-btn flat>like</v-btn>
+  
+        </v-card-actions>
+
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
+      </v-flex>      
+          </v-layout>
   
 </div>
 </template>
@@ -21,6 +49,7 @@ import store from "../store"
     },
     data: function () {
             return {
+              show: false,
               address: ''
             }
   },
